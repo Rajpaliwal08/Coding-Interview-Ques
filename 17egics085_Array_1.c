3d48dc03d8af432058f0b0fea4581c227420a4be
rajpaliwal08 <rajpaliwal0108@gmail.com>
2020-04-17 07:54:05 +0530

    Task Completed

---
+++ 17egics085_Array_1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main(){
+	
+	int arr[20];
+	int i,n;
+	
+	printf("Enter the size of array: ");
+	scanf("%d",&n);
+	printf("Enter the elements:\n");
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	frequency(arr, n);
+}
+
+void frequency(int arr[], int n){
+	
+	int freq[n],i,max,temp;
+	
+	for(i=0; i<n; i++){
+		freq[i] = 0;
+	}
+	
+	for(i=0; i<n; i++){
+		freq[arr[i]]++;
+	}
+	
+	max=freq[0];
+	for(i=0; i<n; i++){
+		if(freq[i]!=0){
+			if(freq[i]>max){
+				max=freq[i];
+				temp=i;
+			}
+		}
+	}
+	printf("%d",temp);
+	
+}
